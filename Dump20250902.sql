ccbd76b78fcd31f6ebe46fa478e17b0a1c95f0f3
Menou-Alexandre <menou.alexandre@gmail.com>
2025-09-02 10:44:08 +0200

    Ajouté les courses et les users. Aussi changé la table courses.

--- Dump20250902.sql
+++ Dump20250902.sql
@@ -26,11 +26,11 @@ DROP TABLE IF EXISTS `courses`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `courses` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(45) NOT NULL,
+  `title` varchar(255) NOT NULL,
   `category` varchar(45) NOT NULL,
-  `price` varchar(45) NOT NULL,
+  `price` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,6 +39,7 @@ CREATE TABLE `courses` (
 
 LOCK TABLES `courses` WRITE;
 /*!40000 ALTER TABLE `courses` DISABLE KEYS */;
+INSERT INTO `courses` VALUES (1,'Comment ouvrir une porte','Essentiel',999999999),(2,'Comment calculer 1 + 1','Dev',3),(3,'Il faut pas prendre les gens pour des cons mais il faut pas oublier qu\'ils le sont','Marketing',499),(4,'Utiliser un polaroid quand on a accès à un smartphone','Photo...',297),(5,'Faire une base de donnée sql qui put pas du cul en 3 étapes','Dev',123),(6,'Différentier un hibou et une corde','Essentiel',420),(7,'Comment faire rire même si vous êtes pas drôle','Marketing',6000),(8,'Réaliser un sacrifice humain pour appaiser l\'esprit de l\'ordinateur','Dev',666),(9,'A l\'aide, j\'ai accidentellement rogné ma femme de la photo de famille','Photo...',72),(10,'Communiquer avec des petits homme verts pour percer sur Facebook','Marketing',98);
 /*!40000 ALTER TABLE `courses` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -116,7 +117,7 @@ CREATE TABLE `users` (
   `email` varchar(255) NOT NULL,
   `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -125,7 +126,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Jean-Paul-Patric','jppdu42@wanadoo.fr','2025-09-02 07:55:14'),(2,'Gerard','gege12345@free.fr','2025-09-02 08:02:31'),(3,'Robert','robertolerigolo@yahoo.com','2025-09-02 08:02:31'),(4,'Geraldine','gege123456@free.fr','2025-09-02 08:02:31'),(5,'Cebastien','cebastienavecunc@orange.fr','2025-09-02 08:02:31'),(6,'Paul','michel54@yahoo.com','2025-09-02 08:02:31'),(7,'Michel','paul54@yahoo.com','2025-09-02 08:02:31'),(8,'Fabien','nunu6137@wanadoo.fr','2025-09-02 08:02:31'),(9,'Paul','levraimichel54@yahoo.com','2025-09-02 08:02:31'),(10,'Robert','robert.robert@wanadoo.fr','2025-09-02 08:02:31');
+INSERT INTO `users` VALUES (1,'Jean-Paul-Patric','jppdu42@wanadoo.fr','2025-09-02 07:55:14'),(2,'Gerard','gege12345@free.fr','2025-09-02 08:02:31'),(3,'Robert','robertolerigolo@yahoo.com','2025-09-02 08:02:31'),(4,'Geraldine','gege123456@free.fr','2025-09-02 08:02:31'),(5,'Cebastien','cebastienavecunc@orange.fr','2025-09-02 08:02:31'),(6,'Paul','michel54@yahoo.com','2025-09-02 08:02:31'),(7,'Michel','paul54@yahoo.com','2025-09-02 08:02:31'),(8,'Fabien','nunu6137@wanadoo.fr','2025-09-02 08:02:31'),(9,'Paul','levraimichel54@yahoo.com','2025-09-02 08:02:31'),(10,'Robert','robert.robert@wanadoo.fr','2025-09-02 08:02:31'),(11,'Fabio','fabulousfabio@gmail.com','2025-09-02 08:21:53'),(12,'Loic','lolololo@yahoo.fr','2025-09-02 08:21:53'),(13,'Katherine','catherineavecunk@gmail.com','2025-09-02 08:21:53'),(14,'Jean-Jacqueline','jjacqueline@free.fr','2025-09-02 08:21:53'),(15,'Michel','michoudu62@free.fr','2025-09-02 08:21:53'),(16,'Michelle','michoudu63@yahou.fr','2025-09-02 08:21:53'),(17,'Michelline','michoudu64@wanadoo.fr','2025-09-02 08:21:53'),(18,'Michel','michoudu62wastaken@gmail.com','2025-09-02 08:21:53'),(19,'Rodrigo','si123456789@gmail.com','2025-09-02 08:21:53'),(20,'Jean-Pierre','ultimebafouille2@gmail.com','2025-09-02 08:21:53');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -138,4 +139,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-09-02 10:04:28
+-- Dump completed on 2025-09-02 10:41:32
